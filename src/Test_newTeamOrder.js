de8325247dd066ecc038832c13b5f2baa4691517
zoltan_almodo <zoltanalmodo@gmail.com>
2018-07-19 15:39:27 +0100

    last_001

--- src/Test_newTeamOrder.js
+++ src/Test_newTeamOrder.js
@@ -1,21 +1,16 @@
 {
+        // make new teams order !!!
         
-let oldOrder = [0, 1, 2, 3, 4, 5, 6, 7];
-
-function generateNewOrder(oldArray) {
-
-        var newTeamOrder = [];
-
-        for (var i=0; i>oldArray.length; i++) {
-
-                let randomIndex = Math.floor(Math.random() * oldArray.length);
-
-                newTeamOrder.push(oldArray[randomIndex]);
-                oldOrder.splice(randomIndex, 1);
-
-        };
-}
-
-generateNewOrder(oldOrder);
-
+        let oldOrder = [0, 1, 2, 3, 4, 5, 6, 7];
+        
+        function generateNewOrder(oldArray) {        
+                var newTeamOrder = [];        
+                for (var i=0; i=8; i--) {        
+                        let randomIndex = Math.floor(Math.random() * oldArray.length);
+                        newTeamOrder.push(oldArray[randomIndex]);
+                        oldOrder.splice(randomIndex, 1);
+                };
+        }
+        
+        generateNewOrder(oldOrder);
 }
